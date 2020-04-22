2ecdd91543964fb5ea04375b839900ccf0ed5e3b
Uzie Shogo <ujiie@kogecha.naist.jp>
2020-04-23 00:24:08 +0900

    change abb

--- utils/expand_abbrev.py
+++ utils/expand_abbrev.py
@@ -4,55 +4,11 @@ import pickle
 from fairseq.models.transformer import TransformerModel
 
 class Converter(object):
-    def __init__(self, med_dic, convert_type, translater):
+    def __init__(self, med_dic, translater):
         self.med_dic = med_dic
-        self.convert_type = convert_type
         self.translater = translater
 
-    def convert(self, com):
-        if self.convert_type == 'all':
-            return self.convert_alphabet_to_ja_allpath(com)
-        else:
-            return self.convert_alphabet_to_ja(com)
-
-    def convert_alphabet_to_ja(self, sent):
-        sent = mojimoji.zen_to_han(sent, kana=False, digit=False)
-        iters = re.finditer(r'([a-zA-Z][a-zA-Z\s]*)', sent)
-        output_word = ""
-        pos = 0
-        for i in iters:
-            s_pos, e_pos = i.span()
-            word = i.groups()[0]
-            word = re.sub('^\s', r'', word)
-            word = re.sub('\s$', r'', word)
-            s_word = ""
-
-            while pos < s_pos:
-                output_word += sent[pos]
-                pos += 1
-
-            if word in self.med_dic:
-                s_word = self.med_dic[word]
-            elif word.lower() in self.med_dic:
-                s_word = self.med_dic[word.lower()]
-            else:
-                if self.translater:
-                    s_word = self.translater.translate(' '.join(list(mojimoji.zen_to_han(word).lower()))).replace(' ', '')
-                else:
-                    s_word = word
-
-            if s_word == '':
-                s_word = word
-
-            output_word += s_word
-            pos = e_pos
-        while pos < len(sent):
-            output_word += sent[pos]
-            pos += 1
-
-        return [mojimoji.han_to_zen(output_word)]
-
-    def convert_alphabet_to_ja_allpath(self, sent):
+    def convert(self, sent):
         sent = mojimoji.zen_to_han(sent, kana=False, digit=False)
         iters = re.finditer(r'([a-zA-Z][a-zA-Z\s]*)', sent)
         output_word = [""]
@@ -75,9 +31,6 @@ class Converter(object):
             elif self.translater:
                 s_word = [self.translater.translate(' '.join(list(mojimoji.zen_to_han(word).lower()))).replace(' ', '')]
             else:
-                s_word = [word]
-
-            if s_word == ['']:
                 s_word = []
 
             s_word = list(set([word] + s_word))
@@ -88,6 +41,7 @@ class Converter(object):
             output_word = sum(tmp, [])
 
             pos = e_pos
+
         while pos < len(sent):
             output_word = [token + sent[pos] for token in output_word]
             pos += 1
